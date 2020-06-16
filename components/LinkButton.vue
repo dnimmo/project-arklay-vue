1f6bc870d04b5e8aa6feeeb66ff6160c7efcdb81
David Nimmo <dnimmo@gmail.com>
2020-06-16 23:11:29 +0100

    added LinkButton component

---
+++ components/LinkButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <nuxt-link 
+    class="button"
+    :class="disabled ? 'disabled' : 'enabled'"
+    :to="to"
+  >
+    {{text}}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    display: block;
+    margin-top: 40px;
+    width: 60vw;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 10px 20px;
+    border-radius: 5px;
+    background-color: darkslategrey;
+    color: #fafafa;
+    font-size: 16px;
+    cursor: pointer;
+    margin-bottom: 20px;
+    border-width: 2px;
+    border-color: #fafafa;
+    transition: all 0.2s;
+    text-decoration: none;
+
+    &:hover {
+        border-color: darkslategrey;
+        color: darkslategrey;
+        background-color: #fafafa;
+    }
+
+    &.disabled {
+      opacity: 0.3;
+      cursor: not-allowed;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,22 @@
   <div>
     <h1 class="mainTitle">Project Arklay</h1>
     <p class="introText">Your head hurts. You&apos;re not sure where you are, and you definitely don&apos;t know how you got here. There&apos;s rain thrashing the ground all around you. You figure you might as well try and understand what the Hell is going on...</p>
-    <nuxt-link
+    <link-button
       class="button"
-      to="/menu">
-      Begin
-    </nuxt-link>
+      to="/menu"
+      text="Begin"
+    />
   </div>
 </template>
 
 <script>
+import LinkButton from '~/components/LinkButton'
+
 export default {
-  layout: 'main'
+  layout: 'main',
+  components: {
+    LinkButton,
+  }
 }
 </script>
 
@@ -29,29 +34,6 @@ export default {
   .mainTitle {
     padding: 10vw 0;
     margin: 0;
-  }
-
-  .introText {
-    padding: 5vw;
-  }
-
-  .button {
-    padding: 10px 20px;
-    border-radius: 5px;
-    background-color: darkslategrey;
-    color: #fafafa;
-    font-size: 16px;
-    cursor: pointer;
-    margin-bottom: 20px;
-    border-width: 2px;
-    border-color: #fafafa;
-    transition: all 0.2s;
-    
-    &:hover {
-        border-color: darkslategrey;
-        color: darkslategrey;
-        background-color: #fafafa;
-    }
   }
   
 </style>

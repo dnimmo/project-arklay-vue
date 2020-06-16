725c9e97fed0452ef074e0a78b33fc4dd85a127a
David Nimmo <dnimmo@gmail.com>
2020-06-16 22:20:30 +0100

    added layout for re-use

---
+++ layouts/main.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="siteWrapper">
+    <nuxt />
+  </main>
+</template>
+
+<style lang="scss">
+  .body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .siteWrapper {
+    font-family: 'Source Code Pro', monospace;
+    background-color:slategrey;
+    color: #fafafa;
+    text-align: center;
+    min-height: 90vh;
+    padding: 10vw;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,58 @@
 <template>
-  <div class="index wrapper">
-    <h1 class="index mainTitle">Project Arklay</h1>
-    <p>Your head hurts. You&apos;re not sure where you are, and you definitely don&apos;t know how you got here. There&apos;s rain thrashing the ground all around you. You figure you might as well try and understand what the Hell is going on...</p>
+  <div>
+    <h1 class="mainTitle">Project Arklay</h1>
+    <p class="introText">Your head hurts. You&apos;re not sure where you are, and you definitely don&apos;t know how you got here. There&apos;s rain thrashing the ground all around you. You figure you might as well try and understand what the Hell is going on...</p>
+    <nuxt-link
+      class="button"
+      to="/menu">
+      Begin
+    </nuxt-link>
   </div>
 </template>
 
-<style lang="scss">
-  /* This is _not_ scoped on purpose in order to style the `body` tag. Until you find a better solution for this, don't use generic names here */
-  .body {
-    box-sizing: border-box;
+<script>
+export default {
+  layout: 'main'
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    font-family: 'Source Code Pro', monospace;
+    background-color:slategrey;
+    color: #fafafa;
+    text-align: center;
+    min-height: 90vh;
+    padding: 10vw;
+  }
+
+  .mainTitle {
+    padding: 10vw 0;
     margin: 0;
-    padding: 0;
   }
 
-  .index {
-    &.wrapper {
-      font-family: 'Source Code Pro', monospace;
-      background-color:slategrey;
-      color: #fafafa;
-      text-align: center;
-      min-height: 90vh;
-      padding: 10vw;
-    }
+  .introText {
+    padding: 5vw;
+  }
 
-    &.mainTitle {
-      padding: 10vw 0;
-      margin: 0;
+  .button {
+    padding: 10px 20px;
+    border-radius: 5px;
+    background-color: darkslategrey;
+    color: #fafafa;
+    font-size: 16px;
+    cursor: pointer;
+    margin-bottom: 20px;
+    border-width: 2px;
+    border-color: #fafafa;
+    transition: all 0.2s;
+    
+    &:hover {
+        border-color: darkslategrey;
+        color: darkslategrey;
+        background-color: #fafafa;
     }
   }
+  
 </style>
 

36e05417dd77d200142a42a5bd76cce78e02587b
David Nimmo <dnimmo@gmail.com>
2020-06-19 11:04:59 +0100

    animate message appearing

--- components/Message.vue
+++ components/Message.vue
@@ -1,5 +1,10 @@
 <template>
-  <p class="message">{{ text }}</p>
+  <p 
+    class="message"
+    :class="text !== '' ? 'animated' : 'notAnimated'"
+  >
+    {{ text }}
+  </p>
 </template>
 
 <script>
@@ -14,8 +19,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @keyframes appear {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
   .message {
     min-height: 20px;
     padding: 0 10vw;
+
+    &.animated {
+      animation: appear 0.2s ease-in-out;
+    }
   }
 </style>
